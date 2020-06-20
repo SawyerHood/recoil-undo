fb11987c912fe6d0e6b5e49892126794a415ca1f
Sawyer Hood <sawyerjhood@gmail.com>
2020-06-20 12:55:07 -0700

    Basic working version

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { RecoilRoot, atom, useRecoilState } from 'recoil';
+import { RecoilUndoRoot, useUndo } from 'recoil-undo';
 
-import { ExampleComponent } from 'recoil-undo'
-import 'recoil-undo/dist/index.css'
+const COUNT = atom({
+  default: 0,
+  key: 'count',
+});
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <RecoilRoot>
+      <RecoilUndoRoot>
+        <Counter />
+      </RecoilUndoRoot>
+    </RecoilRoot>
+  );
+};
+
+function Counter() {
+  const [count, setCount] = useRecoilState(COUNT);
+  const undo = useUndo();
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <div>
+        <Button onClick={() => setCount((count) => count - 1)}>-</Button>
+        {count}
+        <Button onClick={() => setCount((count) => count + 1)}>+</Button>
+      </div>
+      <Button onClick={undo}>Undo</Button>
+    </div>
+  );
 }
 
-export default App
+function Button(props: { onClick: () => void; children: React.ReactNode }) {
+  const style = {
+    margin: '12px 12px',
+  };
+  return (
+    <button style={style} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,82 @@
-import * as React from 'react'
-import styles from './styles.module.css'
+import React, {
+  useState,
+  useCallback,
+  useRef,
+  useMemo,
+  useContext,
+} from 'react';
+import {
+  useRecoilTransactionObserver_UNSTABLE,
+  Snapshot,
+  useRecoilSnapshot,
+  useGotoRecoilSnapshot,
+} from 'recoil';
 
-interface Props {
-  text: string
-}
+type History = {
+  past: Snapshot[];
+  present: Snapshot;
+  future: Snapshot[];
+};
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+type ContextState = {
+  undo: () => void;
+};
+
+const UndoContext = React.createContext<ContextState>({ undo: () => {} });
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+export const RecoilUndoRoot = React.memo(
+  ({ children }: Props): React.ReactElement => {
+    const currentSnapshot = useRecoilSnapshot();
+    const [history, setHistory] = useState<History>({
+      past: [],
+      present: currentSnapshot,
+      future: [],
+    });
+    const gotoSnapshot = useGotoRecoilSnapshot();
+    const isUndoingRef = useRef<boolean>(false);
+
+    useRecoilTransactionObserver_UNSTABLE(({ snapshot, previousSnapshot }) => {
+      if (isUndoingRef.current) {
+        isUndoingRef.current = false;
+        return;
+      }
+      setHistory({
+        past: [...history.past, previousSnapshot],
+        present: snapshot,
+        future: [],
+      });
+    });
+
+    const undo = useCallback(() => {
+      setHistory((history: History) => {
+        if (!history.past.length) {
+          return history;
+        }
+
+        isUndoingRef.current = true;
+        gotoSnapshot(history.past[history.past.length - 1]);
+
+        return {
+          past: history.past.slice(0, history.past.length - 1),
+          present: history.past[history.past.length - 1],
+          future: [history.present, ...history.future],
+        };
+      });
+    }, [setHistory, gotoSnapshot]);
+
+    const value = useMemo(() => ({ undo }), [undo]);
+
+    return (
+      <UndoContext.Provider value={value}>{children}</UndoContext.Provider>
+    );
+  },
+);
+
+export function useUndo(): () => void {
+  const { undo } = useContext(UndoContext);
+  return undo;
 }
