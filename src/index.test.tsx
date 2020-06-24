7dcb946a64001a51a947f0fbd0839a00470e1f4b
Sawyer Hood <sawyerjhood@gmail.com>
2020-06-23 19:11:17 -0700

    Add a basic unit test for demo example

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,93 @@
-import { ExampleComponent } from '.'
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { useUndo, useRedo, RecoilUndoRoot } from './index';
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
 
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
-  })
-})
+const COUNT = atom({
+  default: 0,
+  key: 'count',
+});
+
+const TWO_TIMES = selector({
+  get: ({ get }) => get(COUNT) * 2,
+  key: 'two_times',
+});
+
+const App = () => {
+  return (
+    <RecoilRoot>
+      <RecoilUndoRoot trackedAtoms={[COUNT]}>
+        <Counter />
+      </RecoilUndoRoot>
+    </RecoilRoot>
+  );
+};
+
+function Counter() {
+  const [count, setCount] = useRecoilState(COUNT);
+  const double = useRecoilValue(TWO_TIMES);
+  const undo = useUndo();
+  const redo = useRedo();
+  return (
+    <div>
+      <div>
+        <button
+          data-testid='dec'
+          onClick={() => setCount((count) => count - 1)}
+        >
+          -
+        </button>
+        <span data-testid='count'>{count}</span>
+        <button
+          data-testid='inc'
+          onClick={() => setCount((count) => count + 1)}
+        >
+          +
+        </button>
+      </div>
+      <button data-testid='undo' onClick={undo}>
+        Undo
+      </button>
+      <button data-testid='redo' onClick={redo}>
+        Redo
+      </button>
+      <div>{double}</div>
+    </div>
+  );
+}
+
+describe('recoil-undo', () => {
+  it('handles a simple undo and redo case', () => {
+    const { getByTestId } = render(<App />);
+    const inc = getByTestId('inc');
+    const dec = getByTestId('dec');
+    const count = getByTestId('count');
+    const undoButton = getByTestId('undo');
+    const redoButton = getByTestId('redo');
+
+    const plus = () => fireEvent.click(inc);
+    const minus = () => fireEvent.click(dec);
+    const getCount = () => Number(count.textContent);
+    const undo = () => fireEvent.click(undoButton);
+    const redo = () => fireEvent.click(redoButton);
+
+    plus();
+    expect(getCount()).toBe(1);
+
+    plus();
+    plus();
+    minus();
+    expect(getCount()).toBe(2);
+    undo();
+    expect(getCount()).toBe(3);
+    redo();
+    expect(getCount()).toBe(2);
+  });
+});
