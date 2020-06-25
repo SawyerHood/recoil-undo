8d5e00543c14b1ecf86f52ddd1f39b6517050962
Sawyer Hood <sawyerjhood@gmail.com>
2020-06-25 09:38:28 -0700

    Refactor tests

---
+++ src/test_utils/examples.tsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { useUndo, useRedo, RecoilUndoRoot } from '../index';
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+  RecoilState,
+} from 'recoil';
+
+export const COUNT = atom({
+  default: 0,
+  key: 'count',
+});
+
+const TWO_TIMES = selector({
+  get: ({ get }) => get(COUNT) * 2,
+  key: 'two_times',
+});
+
+const TEXT = atom({
+  default: '',
+  key: 'text',
+});
+
+type Props = { trackedAtoms?: RecoilState<any>[] };
+
+const App = (props: Props) => {
+  return (
+    <RecoilRoot>
+      <RecoilUndoRoot {...props}>
+        <Counter />
+      </RecoilUndoRoot>
+    </RecoilRoot>
+  );
+};
+
+function Counter() {
+  const [count, setCount] = useRecoilState(COUNT);
+  const double = useRecoilValue(TWO_TIMES);
+  const [text, setText] = useRecoilState(TEXT);
+  const undo = useUndo();
+  const redo = useRedo();
+  return (
+    <div>
+      <div>
+        <button
+          data-testid='dec'
+          onClick={() => setCount((count) => count - 1)}
+        >
+          -
+        </button>
+        <span data-testid='count'>{count}</span>
+        <button
+          data-testid='inc'
+          onClick={() => setCount((count) => count + 1)}
+        >
+          +
+        </button>
+      </div>
+      <button data-testid='undo' onClick={undo}>
+        Undo
+      </button>
+      <button data-testid='redo' onClick={redo}>
+        Redo
+      </button>
+      <div data-testid='countx2'>{double}</div>
+      <input
+        data-testid='text'
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </div>
+  );
+}
+
+export function renderCounter(props: Props = {}) {
+  const queries = render(<App />);
+  const inc = queries.getByTestId('inc');
+  const dec = queries.getByTestId('dec');
+  const count = queries.getByTestId('count');
+  const undoButton = queries.getByTestId('undo');
+  const redoButton = queries.getByTestId('redo');
+  const countx2 = queries.getByTestId('countx2');
+  const text = queries.getByTestId('text') as HTMLInputElement;
+
+  const plus = () => fireEvent.click(inc);
+  const minus = () => fireEvent.click(dec);
+  const getCount = () => Number(count.textContent);
+  const undo = () => fireEvent.click(undoButton);
+  const redo = () => fireEvent.click(redoButton);
+  const getCountx2 = () => Number(countx2.textContent);
+  const getText = () => text.value;
+  const typeText = (value: string) =>
+    fireEvent.change(text, { target: { value } });
+
+  return {
+    plus,
+    minus,
+    getCount,
+    undo,
+    redo,
+    getCountx2,
+    getText,
+    typeText,
+    queries,
+  };
+}
